d2fbc0e8b933a2b49760c3ec01a2b327f8aad1d3
umarazam <umarazam011@gmail.com>
2023-06-06 19:08:45 +0500

    1. View button added.

--- custom/ecare_invoicing/models/patient_invoice_history.py
+++ custom/ecare_invoicing/models/patient_invoice_history.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, _
 
 
 class PatientInvoiceHistory(models.TransientModel):
@@ -22,4 +22,19 @@ class PatientInvoiceHistory(models.TransientModel):
     payment_state = fields.Char(string="Status")
 
     amount = fields.Float(string="Subtotal")
+
+    def action_view_record(self):
+        view_id = self.env.ref('account.view_move_form').id
+        if self.invoice_type == 'Refund':
+            view_id = self.env.ref('ecare_invoicing.refund_view_move_form').id
+
+        return{
+            "name": _("Invoice"),
+            'type': 'ir.actions.act_window',
+            'view_mode': 'form',
+            'view_id': view_id,
+            'res_model': 'account.move',
+            'res_id': self.move_id.id,
+            'context': {'default_partner_id': self.patient_id.partner_id.id},
+        }
 
