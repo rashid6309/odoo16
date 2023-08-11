a0d13146d58d4da037059fb4300d984bafea63a4
umarazam <umarazam011@gmail.com>
2023-08-11 13:36:52 +0500

    1. Right margin applied. 2. Mr number center aligned. 3. Break line not working.

--- custom/ecare_core/models/ec_patient.py
+++ custom/ecare_core/models/ec_patient.py
@@ -456,7 +456,3 @@ class EcarePatient(models.Model):
             self.env['third.party.api.log'].sudo().create(api_log_values)
         except Exception as e:
             _logger.warning(e)
-
-    def action_patient_profile_print_report(self):
-        return self.env.ref('ecare_reporting.ec_patient_profile_report').report_action(self)
-
