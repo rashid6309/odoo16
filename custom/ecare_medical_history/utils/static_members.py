c9011a39b571f76e6acbe2f4b3284955365122dc
umarazam <umarazam011@gmail.com>
2023-08-17 09:23:21 +0500

    1. Bug fixed.

--- custom/ecare_medical_history/utils/static_members.py
+++ custom/ecare_medical_history/utils/static_members.py
@@ -156,8 +156,8 @@ class StaticMember:
         ('none', 'None'),
     ]
 
-    SIDE = [('right', "Right",
-             'left', "Left")]
+    SIDE = [('right', "Right"),
+            ('left', "Left")]
 
     SIZE = [('normal', "Normal"),
             ('small', "Small"),
