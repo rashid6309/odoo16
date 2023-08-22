7eed1f791dd0489de3796d6456be3c9ba4a28b84
rashid <rashid.noor@heurixtics.com>
2023-08-22 11:30:39 +0500

    Physical Examination.

---
+++ custom/ecare_medical_history/models/physical_examination.py
@@ -0,0 +1,24 @@
+from odoo import api, models, fields, _
+
+from odoo.addons.ecare_medical_history.utils.static_members import StaticMember
+
+
+class MedicalPhysicalExamination(models.Model):
+    _name = "ec.medical.physical.examination"
+    _description = "Patient Physical Examination"
+
+    consultation_id = fields.Many2one(comodel_name='ec.first.consultation', string='First Consultation')
+
+    female_weight = fields.Char('Weight')
+    female_height = fields.Char('Height')
+    female_bmi = fields.Char('BMI')
+    female_bp_upper = fields.Char('B.P')
+    female_bp_lower = fields.Char('B.P')
+    female_pulse = fields.Char('Pulse')
+    female_temperature = fields.Char('Temperature')
+    female_thyroid = fields.Selection(selection=StaticMember.THYROID, string='Thyroid')
+    female_thyroid_goitear_length = fields.Char('Goitear')
+    female_thyroid_goitear_width = fields.Char('Goitear')
+    female_thyroid_goitear_type = fields.Selection(selection=StaticMember.THYROID_GOITEAR_TYPE, string='Goitear')
+    female_thyroid_goitear_nodule = fields.Boolean('Nodule')
+    female_pallor = fields.Selection(selection=StaticMember.PALLOR, string='Pallor')
