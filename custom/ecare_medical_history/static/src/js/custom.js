075b0b3fb1743a4972843dfda3d38bc923729ab3
umarazam <umarazam011@gmail.com>
2023-09-19 13:35:07 +0500

    1. Javascript indenetation corrected

--- custom/ecare_medical_history/static/src/js/custom.js
+++ custom/ecare_medical_history/static/src/js/custom.js
@@ -1,78 +1,79 @@
 odoo.define('ecare_medical_history.FirstConsultation', function (require) {
-'use strict';
-const { registry } = require('@web/core/registry');
-const { FormRenderer } = require('@web/views/form/form_renderer');
-const { formView } = require('@web/views/form/form_view');
-const Dialog = require('web.Dialog');
- var rpc = require('web.rpc');
-class FirstConsultationFormController extends FormRenderer {
-   setup() {
-       super.setup();
-       console.log(this.env.config.bannerRoute);
-                var self = this;
-               console.log(this.bannerRoute);
-               const response = rpc.query({
+    'use strict';
+    const { registry } = require('@web/core/registry');
+    const { FormRenderer } = require('@web/views/form/form_renderer');
+    const { formView } = require('@web/views/form/form_view');
+    const Dialog = require('web.Dialog');
+    var rpc = require('web.rpc');
+
+    class FirstConsultationFormController extends FormRenderer {
+        setup() {
+            super.setup();
+            console.log(this.env.config.bannerRoute);
+            var self = this;
+            console.log(this.bannerRoute);
+            const response = rpc.query({
                 route: this.env.config.bannerRoute,
                 params: [11],
             });
 
-   }
-   _MoreOptions(){
+        }
+        _MoreOptions() {
             console.log(this);
-                   var dialog = new Dialog(null, {
-                       title: "js class blog",
-                       size: 'medium',
-                       $content: $('<div/>', {
-                           html: 'hai'
-                       }),
-                       buttons: [{
-                           text: "Close",
-                           close: true
-                       }]
-                   });
-                   dialog.open();
-   }
-}
+            var dialog = new Dialog(null, {
+                title: "js class blog",
+                size: 'medium',
+                $content: $('<div/>', {
+                    html: 'hai'
+                }),
+                buttons: [{
+                    text: "Close",
+                    close: true
+                }]
+            });
+            dialog.open();
+        }
+    }
 
-const PickingFormView = {
-   ...formView,
-   Renderer: FirstConsultationFormController,
-};
-registry.category("views").add("banner_summary_route", PickingFormView);
+    const PickingFormView = {
+        ...formView,
+        Renderer: FirstConsultationFormController,
+    };
+    registry.category("views").add("banner_summary_route", PickingFormView);
 
 });
 
 
 odoo.define('ecare_medical_history.owl_test', function (require) {
-"use strict";
-   const AbstractField = require('web.AbstractFieldOwl');
-   const fieldRegistry = require('web.field_registry_owl');
+    "use strict";
+    // const AbstractField = require('web.AbstractFieldOwl');
+    // const fieldRegistry = require('web.field_registry_owl');
     const { registry } = require('@web/core/registry');
     const { useInputField } = require("@web/views/fields/input_field_hook");
     const { useService } = require("@web/core/utils/hooks");
 
 
-    var time = require('web.time');
-    var rpc = require('web.rpc');
-    var translation = require('web.translation');
-    var _t = translation._t;
-    const { Component,useRef} = owl;
-    const { CharField }  = require("@web/views/fields/char/char_field");
+    // var time = require('web.time');
+    // var rpc = require('web.rpc');
+    // var translation = require('web.translation');
+    // var _t = translation._t;
+    // const { CharField }  = require("@web/views/fields/char/char_field");
 
-     class DomainSelectorTextField extends Component {
+    const { Component, useRef } = owl;
 
-       setup(){
-           super.setup();
-           this.action = useService("action");
-           this.input = useRef('inputdate')
-           useInputField({ getValue: () => this.props.value || "", refName: "inputdate" });
-           useInputField({ getValue: () => this.props.record.data || "", refName: "patient_data" });
-       }
+    class DomainSelectorTextField extends Component {
+        setup() {
+            super.setup();
+            this.action = useService("action");
+            this.input = useRef('inputdate')
+            useInputField({ getValue: () => this.props.value || "", refName: "inputdate" });
+            useInputField({ getValue: () => this.props.record.data || "", refName: "patient_data" });
+        }
 
 
-       async _onEditPatientProfile(){
-       console.log(this.props.record.data.patient_id);
-       var patient = this.props.record.data.patient_id;
+        async _onEditPatientProfile() {
+            console.log(this.props.record.data.patient_id);
+            var patient = this.props.record.data.patient_id;
             this.action.doActionButton({
                 type: "object",
                 resId: this.props.value[0],
@@ -82,6 +83,8 @@ odoo.define('ecare_medical_history.owl_test', function (require) {
                 onClose: async () => {
                     await this.props.record.model.root.load();
                     this.props.record.model.notify();
+
+
                 },
             });
         }
