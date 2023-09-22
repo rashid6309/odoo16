554d012b7821cd4749acca15a18f220400d2f9b5
rashid <rashid.noor@heurixtics.com>
2023-09-22 19:00:39 +0500

    Timeline changes Previous Treatment

---
+++ custom/ecare_medical_history/models/previous_treatment.py
@@ -0,0 +1,17 @@
+from odoo import api, models, fields, _
+from odoo.addons.ecare_medical_history.utils.static_members import StaticMember
+
+
+class FemaleGynaecologicalHistory(models.Model):
+    _name = 'ec.medical.previous.treatment'
+    _description = "Previous Treatment"
+
+    first_consultation_id = fields.Many2one('ec.first.consultation', string='First Consultation')
+    type = fields.Selection(selection=StaticMember.PREVIOUS_TREATMENT_TYPE, default='ovulation_induction_intercourse',
+                            string='Type')
+    consultant = fields.Char(string='Consultant')
+    oral_drugs = fields.Char(string='Oral Drugs')
+    down_regulation = fields.Char(string='Down Regulation')
+    superovulation = fields.Char(string='Superovulation')
+    ovarian_response = fields.Char(string='Ovarian Response')
+    outcome = fields.Char(string='Outcome')
