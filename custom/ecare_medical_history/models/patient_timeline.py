7a7ca976e68b39eff69bd34282a968cabeb16087
umarazam <umarazam011@gmail.com>
2023-09-28 12:48:38 +0500

    1. Code refactored for computing the family history of the patient.

--- custom/ecare_medical_history/models/patient_timeline.py
+++ custom/ecare_medical_history/models/patient_timeline.py
@@ -29,143 +29,77 @@ class PatientTimeline(models.Model):
     def _compute_female_values(self):
         pass
 
+    @staticmethod
+    def _compute_patient_family_history(family_history, fields_to_process):
+        if not family_history:
+            return None
+
+        family_history_text = []
+
+        for field_name, (field_label, custom_field_name) in fields_to_process.items():
+            field_records = getattr(family_history, field_name)
+            custom_field = getattr(family_history, custom_field_name)
+            if field_name in ('male_family_history_other', 'female_family_history_other'):
+                family_members_list = field_records
+                if family_members_list:
+                    field_text = f'<strong style="font-weight: 700;">{field_label}:</strong>({family_members_list})'
+                    family_history_text.append(field_text)
+            elif field_records or (custom_field and custom_field.strip()):
+                custom_text = f' ({custom_field.strip()})' if custom_field else ''
+                family_members_list = [rec.name for rec in field_records]
+                if family_members_list:
+                    field_text = f'<strong style="font-weight: 700;">{field_label}:</strong>({", ".join(family_members_list)}){custom_text}'
+                    family_history_text.append(field_text)
+
+        if family_history_text:
+            family_history_text = '<br>'.join(family_history_text)
+            return family_history_text
+
+        return None
+
     def _compute_family_history(self):
         # Male Fields Processing
 
-        family_history = self.ec_first_consultation_id.ec_male_family_history_id
-        if family_history:
-            fields_to_process = {
-                'male_diabetes_ids': ('Diabetes', 'male_other_diabetes'),
-                'male_malignancies_ids': ('Malignancies', 'male_other_malignancies'),
-                'male_hypertension_ids': ('Hypertension', 'male_other_hypertension'),
-                'male_mental_illness_ids': ('Mental Illness', 'male_other_mental_illness'),
-                'male_tuberculosis_ids': ('Tuberculosis', 'male_other_tuberculosis'),
-                'male_abnormalities_ids': ('Abnormalities', 'male_other_abnormalities'),
-                'male_family_history_other': ('Other History', 'male_family_history_other')
-            }
+        male_fields_to_process = {
+            'male_diabetes_ids': ('Diabetes', 'male_other_diabetes'),
+            'male_malignancies_ids': ('Malignancies', 'male_other_malignancies'),
+            'male_hypertension_ids': ('Hypertension', 'male_other_hypertension'),
+            'male_mental_illness_ids': ('Mental Illness', 'male_other_mental_illness'),
+            'male_tuberculosis_ids': ('Tuberculosis', 'male_other_tuberculosis'),
+            'male_abnormalities_ids': ('Abnormalities', 'male_other_abnormalities'),
+            'male_family_history_other': ('Other History', 'male_family_history_other')
+        }
 
-            family_history_text = []
-            for field_name, (field_label, custom_field_name) in fields_to_process.items():
-                field_records = getattr(family_history, field_name)
-                custom_field = getattr(family_history, custom_field_name)
-                if field_name == 'male_family_history_other':
-                    family_members_list = field_records
-                    if family_members_list:
-                        field_text = f'<strong style="font-weight: 700;">{field_label}:</strong>({family_members_list})'
-                        family_history_text.append(field_text)
-                elif field_records or (custom_field and custom_field.strip()):
-                    custom_text = f' ({custom_field.strip()})' if custom_field else ''
-                    family_members_list = [rec.name for rec in field_records]
-                    if family_members_list:
-                        field_text = f'<strong style="font-weight: 700;">{field_label}:</strong>({", ".join(family_members_list)}){custom_text}'
-                        family_history_text.append(field_text)
+        self.male_family_history = PatientTimeline._compute_patient_family_history(
+            family_history=self.ec_first_consultation_id.ec_male_family_history_id,
+            fields_to_process=male_fields_to_process
+        )
 
-            if family_history_text:
-                self.male_family_history = '<br>'.join(family_history_text)
-            else:
-                self.male_family_history = False
-        else:
-            self.male_family_history = False
 
         # Female Fields Processing
 
-        family_history = self.ec_first_consultation_id.ec_female_family_history_id
-        if family_history:
-            fields_to_process = {
-                'female_diabetes_ids': ('Diabetes', 'female_other_diabetes'),
-                'female_malignancies_ids': ('Malignancies', 'female_other_malignancies'),
-                'female_hypertension_ids': ('Hypertension', 'female_other_hypertension'),
-                'female_mental_illness_ids': ('Mental Illness', 'female_other_mental_illness'),
-                'female_tuberculosis_ids': ('Tuberculosis', 'female_other_tuberculosis'),
-                'female_abnormalities_ids': ('Abnormalities', 'female_other_abnormalities'),
-                'female_twins_ids': ('Twins', 'female_other_twins'),
-                'female_pregnancy_induced_hypertension_ids': (
-                    'Pregnancy Induced Hypertension', 'female_other_pregnancy_induced_hypertension'),
-                'female_miscarriage_ids': ('Miscarriages', 'female_other_miscarriage'),
-                'female_preterm_birth_ids': ('Preterm Births', 'female_other_preterm_birth'),
-                'female_still_birth_ids': ('Still Births', 'female_other_still_birth'),
-                'female_pre_mature_ovarian_failure_ids': (
-                    'Pre-mature Ovarian Failure', 'female_other_pre_mature_ovarian_failure'),
-                'female_family_history_other': ('Other History', 'female_family_history_other')
-            }
+        fields_to_process = {
+            'female_diabetes_ids': ('Diabetes', 'female_other_diabetes'),
+            'female_malignancies_ids': ('Malignancies', 'female_other_malignancies'),
+            'female_hypertension_ids': ('Hypertension', 'female_other_hypertension'),
+            'female_mental_illness_ids': ('Mental Illness', 'female_other_mental_illness'),
+            'female_tuberculosis_ids': ('Tuberculosis', 'female_other_tuberculosis'),
+            'female_abnormalities_ids': ('Abnormalities', 'female_other_abnormalities'),
+            'female_twins_ids': ('Twins', 'female_other_twins'),
+            'female_pregnancy_induced_hypertension_ids': (
+                'Pregnancy Induced Hypertension', 'female_other_pregnancy_induced_hypertension'),
+            'female_miscarriage_ids': ('Miscarriages', 'female_other_miscarriage'),
+            'female_preterm_birth_ids': ('Preterm Births', 'female_other_preterm_birth'),
+            'female_still_birth_ids': ('Still Births', 'female_other_still_birth'),
+            'female_pre_mature_ovarian_failure_ids': (
+                'Pre-mature Ovarian Failure', 'female_other_pre_mature_ovarian_failure'),
+            'female_family_history_other': ('Other History', 'female_family_history_other')
+        }
 
-            family_history_text = []
-            for field_name, (field_label, custom_field_name) in fields_to_process.items():
-                field_records = getattr(family_history, field_name)
-                custom_field = getattr(family_history, custom_field_name)
-                if field_name == 'female_family_history_other':
-                    family_members_list = field_records
-                    if family_members_list:
-                        field_text = f'<strong style="font-weight: 700;">{field_label}:</strong>({family_members_list})'
-                        family_history_text.append(field_text)
-                elif field_records or (custom_field and custom_field.strip()):
-                    custom_text = f' ({custom_field.strip()})' if custom_field else ''
-                    family_members_list = [rec.name for rec in field_records]
-                    if family_members_list:
-                        field_text = f'<strong style="font-weight: 700;">{field_label}:</strong>({", ".join(family_members_list)}){custom_text}'
-                        family_history_text.append(field_text)
-
-            if family_history_text:
-                self.female_family_history = '<br>'.join(family_history_text)
-            else:
-                self.female_family_history = False
-        else:
-            self.female_family_history = False
-
-    # def _compute_family_history(self):
-    #     family_history = self.ec_first_consultation_id.ec_male_family_history_id
-    #     if family_history:
-    #         fields_to_process = {
-    #             'male_diabetes_ids': 'Diabetes',
-    #             'male_malignancies_ids': 'Malignancies',
-    #             'male_hypertension_ids': 'Hypertension',
-    #             'male_mental_illness_ids': 'Mental Illness',
-    #             'male_tuberculosis_ids': 'Tuberculosis',
-    #             'male_abnormalities_ids': 'Abnormalities'
-    #         }
-    #
-    #         family_history_text = []
-    #         for field_name, field_label in fields_to_process.items():
-    #             field_records = getattr(family_history, field_name)
-    #             if field_records:
-    #                 family_members_list = [rec.name for rec in field_records]
-    #                 if family_members_list:
-    #                     field_text = f'<strong style="font-weight: 700;">{field_label}:</strong> ({", ".join(family_members_list)})'
-    #                     family_history_text.append(field_text)
-    #
-    #         if family_history_text:
-    #             self.male_family_history = '<br>'.join(family_history_text)
-    #         else:
-    #             self.male_family_history = False
-    #     else:
-    #         self.male_family_history = False
-    #
-    #     family_history = self.ec_first_consultation_id.ec_female_family_history_id
-    #     if family_history:
-    #         fields_to_process = {
-    #             'female_diabetes_ids': 'Diabetes',
-    #             'female_malignancies_ids': 'Malignancies',
-    #             'female_hypertension_ids': 'Hypertension',
-    #             'female_mental_illness_ids': 'Mental Illness',
-    #             'female_tuberculosis_ids': 'Tuberculosis',
-    #             'female_abnormalities_ids': 'Abnormalities'
-    #         }
-    #
-    #         family_history_text = []
-    #         for field_name, field_label in fields_to_process.items():
-    #             field_records = getattr(family_history, field_name)
-    #             if field_records:
-    #                 family_members_list = [rec.name for rec in field_records]
-    #                 if family_members_list:
-    #                     field_text = f'<strong style="font-weight: 700;">{field_label}:</strong> ({", ".join(family_members_list)})'
-    #                     family_history_text.append(field_text)
-    #
-    #         if family_history_text:
-    #             self.female_family_history = '<br>'.join(family_history_text)
-    #         else:
-    #             self.female_family_history = False
-    #     else:
-    #         self.female_family_history = False
+        self.female_family_history = PatientTimeline._compute_patient_family_history(
+            family_history=self.ec_first_consultation_id.ec_female_family_history_id,
+            fields_to_process=fields_to_process
+        )
 
     @api.onchange("timeline_patient_id")
     def populate_dependent_patient_field(self):
