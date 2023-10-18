81c2792fbe479ca61d3d7e639a47406114d1c424
rashid <rashid.noor@heurixtics.com>
2023-10-18 17:43:05 +0500

    1. Seminology is not clickable. phase III 2. infertility type (not applies to First Consultation that is user selected only): Value to be like document and as disucssed by sir. If the data is in obs. then the type is automatically secondary infertility. but it should not be changed on the first consultation. 3. "Patient procedure" needs to be "surgical detail" when clicked on the surgical button 4. Other in family history needs to have placeholder (in both male/female) 5. Missing part in the gynacology examination:- 6. Invisible if the biological DOB is entered. 7. Living Duration year after the field.and with other years and month 8. Bug in mensural cycle and make it drop-down 9. Age at menarche to dropdown (1-32 years) 10. Surgical / Medical in seperate tabs 11. P/S not PS biological dob factor styling age and bilogical dob capitalize placehoder 1st letter capital Days Gap  issue in gynaecological Days Gap Should be selection fields medical and sugircal history in different divs USG (Abd/vag) in genaecological history hide fields which were represent in tabular form

---
+++ custom/ecare_medical_history/models/ec_medical_multi_selection.py
@@ -0,0 +1,10 @@
+from odoo import api, models, fields, _
+from odoo.exceptions import ValidationError
+from odoo.addons.ecare_medical_history.utils.static_members import StaticMember
+
+
+class EcMedicalMultiSelection(models.Model):
+    _name = 'ec.medical.multi.selection'
+
+    name = fields.Char(string='Name')
+    type = fields.Selection(selection=StaticMember.MULTI_SELECTION_FIELD, string='Type')
