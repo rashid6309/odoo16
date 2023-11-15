b2862ea6043ed248b4809b6a89a056449ad26976
Hassan2711 <hassanaamer30@gmail.com>
2023-11-15 14:48:42 +0500

    changed genital examination fields in first consultation male from selection type to many2many

--- custom/ecare_medical_history/models/male_genital_examination.py
+++ custom/ecare_medical_history/models/male_genital_examination.py
@@ -11,13 +11,13 @@ class GenitalExamination(models.Model):
     skin_condition = fields.Char(string="Skin Condition")
 
     # SIDES
-    hernia = fields.Selection(selection=StaticMember.SIDE, )
-    vericocele = fields.Selection(selection=StaticMember.SIDE, )
-    hydrocele = fields.Selection(selection=StaticMember.SIDE, )
-    undescended_testis = fields.Selection(selection=StaticMember.SIDE, )
-    supermatic_cord_normal = fields.Selection(selection=StaticMember.SIDE, )
-    supermatic_cord_thick = fields.Selection(selection=StaticMember.SIDE, )
-    supermatic_cord_tender = fields.Selection(selection=StaticMember.SIDE, )
+    # hernia = fields.Selection(selection=StaticMember.SIDE, )
+    # vericocele = fields.Selection(selection=StaticMember.SIDE, )
+    # hydrocele = fields.Selection(selection=StaticMember.SIDE, )
+    # undescended_testis = fields.Selection(selection=StaticMember.SIDE, )
+    # supermatic_cord_normal = fields.Selection(selection=StaticMember.SIDE, )
+    # supermatic_cord_thick = fields.Selection(selection=StaticMember.SIDE, )
+    # supermatic_cord_tender = fields.Selection(selection=StaticMember.SIDE, )
 
     testicular_size_right = fields.Selection(selection=StaticMember.SIZE, )
     testicular_size_left = fields.Selection(selection=StaticMember.SIZE, )
@@ -25,3 +25,46 @@ class GenitalExamination(models.Model):
     testicular_size_mobility = fields.Selection(selection=StaticMember.CHOICE_YES_NO, )
 
     ec_genital_examination_comment = fields.Char(string="Comment")
+
+    hernia_ids = fields.Many2many(comodel_name='ec.medical.multi.selection',
+                                  relation='genital_examination_multi_selection_hernia',
+                                  column1='genital_examination_id',
+                                  column2='multi_selection_id',
+                                  string='Hernia', domain="[('type', '=', 'hernia')]")
+
+    vericocele_ids = fields.Many2many(comodel_name='ec.medical.multi.selection',
+                                      relation='genital_examination_multi_selection_vericocele',
+                                      column1='genital_examination_id',
+                                      column2='multi_selection_id',
+                                      string='Vericocele', domain="[('type', '=', 'vericocele')]")
+
+    hydrocele_ids = fields.Many2many(comodel_name='ec.medical.multi.selection',
+                                     relation='genital_examination_multi_selection_hydrocele',
+                                     column1='genital_examination_id',
+                                     column2='multi_selection_id',
+                                     string='Hydrocele', domain="[('type', '=', 'hydrocele')]")
+
+    undescended_testis_ids = fields.Many2many(comodel_name='ec.medical.multi.selection',
+                                                  relation='genital_examination_multi_selection_undescended_testis',
+                                                  column1='genital_examination_id',
+                                                  column2='multi_selection_id',
+                                                  string='Undescended Testis',
+                                                  domain="[('type', '=', 'undescended_testis')]")
+
+    supermatic_cord_normal_ids = fields.Many2many(comodel_name='ec.medical.multi.selection',
+                                                  relation='genital_examination_multi_selection_supermatic_cord_normal',
+                                                  column1='genital_examination_id',
+                                                  column2='multi_selection_id',
+                                                  string='Supermatic Cord Normal', domain="[('type', '=', 'supermatic_cord_normal')]")
+
+    supermatic_cord_thick_ids = fields.Many2many(comodel_name='ec.medical.multi.selection',
+                                                 relation='genital_examination_multi_selection_supermatic_cord_thick',
+                                                 column1='genital_examination_id',
+                                                 column2='multi_selection_id',
+                                                 string='Supermatic Cord Thick', domain="[('type', '=', 'supermatic_cord_thick')]")
+
+    supermatic_cord_tender_ids = fields.Many2many(comodel_name='ec.medical.multi.selection',
+                                                  relation='genital_examination_multi_selection_supermatic_cord_tender',
+                                                  column1='genital_examination_id',
+                                                  column2='multi_selection_id',
+                                                  string='Supermatic Cord Tender', domain="[('type', '=', 'supermatic_cord_tender')]")
