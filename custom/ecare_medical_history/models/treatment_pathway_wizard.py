89dc91d5f172cf9908871f08d70bcff00976526b
rashid <rashid.noor@heurixtics.com>
2023-12-29 12:05:54 +0500

    female ot_ti checklist male ot_ti checklist medical consents and risk assessment treatment pathway Red and Blue values check Female BMI

--- custom/ecare_medical_history/models/treatment_pathway_wizard.py
+++ custom/ecare_medical_history/models/treatment_pathway_wizard.py
@@ -6,4 +6,9 @@ class TreatmentPathwayWizard(models.TransientModel):
     _description = "Treatment Pathway Wizard"
 
     message = fields.Text('Message')
+    ec_repeat_consultation_id = fields.Many2one('ec.repeat.consultation')
+
+    def action_understand_wizard(self):
+        if self.ec_repeat_consultation_id:
+            return self.ec_repeat_consultation_id.action_state_to_decision_pending()
 
