2d5729d860ecb43584b054142862288e3b739d3c
rashid <rashid.noor@heurixtics.com>
2024-01-23 14:13:24 +0500

    Header Footer issues fix in report

--- custom/ecare_medical_history/reports/semen_analysis_report.py
+++ custom/ecare_medical_history/reports/semen_analysis_report.py
@@ -1,16 +1,18 @@
-from odoo import models
+from odoo import models, api
+
 
 class SemenAnalysisHospitalRecord(models.AbstractModel):
     _name = 'report.ecare_medical_history.ec_semen_analysis_doc'
     _description = 'Semen Analysis Report: Hospital Record'
 
+    @api.model
     def _get_report_values(self, docids, data=None):
         docs = self.env['ec.semen.analysis'].browse(docids)
 
         return {
             'doc_ids': docs.ids,
             'doc_model': 'ec.semen.analysis',
-            'company': self.env.user.company_id,
+            'company_id': self.env.user.company_id,
             'docs': docs,
         }
 
