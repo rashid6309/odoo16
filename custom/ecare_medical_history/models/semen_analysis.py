e138e9b62f927b6f57839d3f30d0a3173c54d99b
umarazam <umarazam011@gmail.com>
2024-02-02 10:42:35 +0500

    1. [Fixed] female_bmi editable issue with keyboard-tab.

--- custom/ecare_medical_history/models/semen_analysis.py
+++ custom/ecare_medical_history/models/semen_analysis.py
@@ -106,7 +106,9 @@ class SemenAnalysis(models.Model):
     sperm_cryopreservation_strawe = fields.Char('Cryopreservation Strawe')
     sperm_cryopreservation_code = fields.Char('Cryopreservation Code')
 
-    seminologist = fields.Many2one(comodel_name='res.users', string='Seminologist', default=lambda self: self.env.user)
+    seminologist_id = fields.Many2one(comodel_name='res.users', string='Seminologist', default=lambda self: self.env.user)
+    legacy_seminologist = fields.Char(string="Legacy System Seminologist", readonly=1)
+
     special_notes = fields.Char("Special Notes")
 
     seme_analysis_id_dummy = fields.Many2one('ec.semen.analysis')
