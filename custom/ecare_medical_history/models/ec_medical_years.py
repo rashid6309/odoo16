56858c7d62f9d860f5779d8b6105dff8257b480f
rashid <rashid.noor@heurixtics.com>
2024-02-06 11:39:00 +0500

    BUG Fixes

--- custom/ecare_medical_history/models/ec_medical_years.py
+++ custom/ecare_medical_history/models/ec_medical_years.py
@@ -1,4 +1,7 @@
 from odoo import models, api, fields
+from odoo.addons.ecare_medical_history.utils.validation import Validation
+from odoo.exceptions import AccessError, UserError, ValidationError
+import re
 
 
 class EcMedicalYear(models.Model):
@@ -7,14 +10,19 @@ class EcMedicalYear(models.Model):
     _order = 'year desc'
     _rec_name = "year"
 
-    year = fields.Integer('Year', required=True)
+    year = fields.Char('Year', required=True)
 
     @api.model
     def create_medical_year_record(self):
         current_year = fields.Date.today().year
-        year_exists = self.env['ec.medical.year'].search([('year', '=', current_year)])
+        year_exists = self.env['ec.medical.year'].search([('year', '=', str(current_year))])
 
         if not year_exists:
-            self.env['ec.medical.year'].create({'year': current_year})
+            self.env['ec.medical.year'].create({'year': str(current_year)})
 
         return True
+
+    @api.onchange('repeat_pregnancy_bp_upper')
+    def _check_integer_input_year(self):
+        if self.year and not re.match(Validation.REGEX_INTEGER_SIMPLE, self.year):
+            raise UserError(f"Please enter a numeric value in Year.")
