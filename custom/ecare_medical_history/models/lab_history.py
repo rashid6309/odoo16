f59da646c0cc1a730ad04b4a2f764b1b6f481903
rashid <rashid.noor@heurixtics.com>
2024-03-17 04:59:35 +0500

    Attachment issue is fixed.

--- custom/ecare_medical_history/models/lab_history.py
+++ custom/ecare_medical_history/models/lab_history.py
@@ -31,3 +31,13 @@ class LabHistory(models.Model):
     def _check_lab_date(self):
         if self.date:
             return Validation._date_validation(self.date)
+
+    @api.model
+    def create(self, vals):
+        templates = super(LabHistory, self).create(vals)
+        # fix attachment ownership
+        for template in templates:
+            if template.attachment_id:
+                template.attachment_id.write({'res_model': self._name, 'res_id': template.id})
+        return templates
+
