b737585bbce0d8270dae99d2d4110ab7a1d91c39
umarazam <umarazam011@gmail.com>
2024-03-25 16:10:19 +0500

    1. Lab history attachments moved from many2many to many2one due to technical limitation .

--- custom/ecare_medical_history/models/lab_history.py
+++ custom/ecare_medical_history/models/lab_history.py
@@ -14,30 +14,13 @@ class LabHistory(models.Model):
     date = fields.Date(string="Date")
 
     filename = fields.Char(string="Filename")
-    attachment_id = fields.Many2many('ir.attachment', 'lab_history_document_rel',
-                                     'lab_id', 'doc_id', help="If you want to upload any attachment.",
-                                     string='Document')
+
+    attachment_id = fields.Binary(string="Document")
 
     results = fields.Text('Results', required=True)
-
-    # attachment_ids = fields.Many2many(comodel_name="ir.attachment",
-    #                                   column1="lab_id",
-    #                                   column2="attachment_id",
-    #                                   relation="lab_history_attachment_id",
-    #                                   string="Attachments",
-    #                                   help="If you want to upload any attachment.")
 
     @api.onchange('date')
     def _check_lab_date(self):
         if self.date:
             return Validation._date_validation(self.date)
-
-    @api.model
-    def create(self, vals):
-        templates = super(LabHistory, self).create(vals)
-        # fix attachment ownership
-        for template in templates:
-            if template.attachment_id:
-                template.attachment_id.write({'res_model': self._name, 'res_id': template.id})
-        return templates
 
